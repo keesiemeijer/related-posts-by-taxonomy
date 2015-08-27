9b12d707a7c569c8e8ad63e10663d32b3751ce3d
keesiemeijer <keesie.meijer@gmail.com>
2015-08-27 15:58:11 +0200

    Add cached posts to debug information

--- includes/debug.php
+++ includes/debug.php
@@ -15,6 +15,7 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 
 		public $debug             = array();
 		public $results           = array();
+		public $plugin            = 0;
 		public $widget_counter    = 0;
 		public $shortcode_counter = 0;
 
@@ -39,6 +40,11 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 			if ( !( is_super_admin() || current_user_can( 'view_rpbt_debug_results' ) ) ) {
 				return;
 			}
+
+			$this->debug[ 'cached' ] = 'FALSE';
+
+			$this->plugin  = Related_Posts_By_Taxonomy_Defaults::get_instance();
+			$this->cache   = $this->plugin->cache instanceof Related_Posts_By_Taxonomy_Cache;
 
 			// Adds debug link before the widget title.
 			add_filter( 'dynamic_sidebar_params', array( $this, 'widget_params' ), 99 );
@@ -86,14 +92,39 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 			} else {
 				$this->debug[ 'type']           = 'shortcode';
 				$this->debug[ 'shortcode args'] = $args;
-				$args['before_shortcode']       = '<div class="rpbt_shortcode">' . $this->debug_link( 'shortcode' ) . '<br/>';
+				$args['before_shortcode']       = '<div class="rpbt_shortcode">hhh' . $this->debug_link( 'shortcode' ) . '<br/>';
 				$args['after_shortcode']        = '</div>';
+			}
+
+			if ( $this->cache ) {
+				$this->check_cache( $args );
 			}
 
 			// Gets current post terms, taxonomies and post ID.
 			add_filter( 'wp_get_object_terms', array( $this, 'object_terms' ), 99, 4 );
 
 			return $args;
+		}
+
+		function check_cache( $args ) {
+
+			// returns only function args
+			$args = $this->plugin->cache->sanitize_cache_args( $args );
+
+			// Get cached post ids (if they exist)
+			$posts = $this->plugin->cache->get_post_meta( $args );
+
+			if ( is_array( $posts ) ) {
+				$current  = isset( $posts['rpbt_current'] ) ? $posts['rpbt_current'] : array();
+				unset( $posts['rpbt_current'] );
+				$posts = array_keys( $posts );
+				$this->debug[ 'cached' ] = 'TRUE';
+				$this->debug[ 'cached post ids'] = $posts;
+				$this->debug[ 'function args'] = $args;
+				$this->debug[ 'cached taxonomies'] = isset( $current['taxonomies'] ) ? $current['taxonomies'] : '';
+				$this->debug[ 'cached terms'] = isset( $current['related_terms'] ) ? $current['related_terms'] : '';
+				$this->debug[ 'current post id'] = $args['post_id'];
+			}
 		}
 
 
@@ -198,13 +229,14 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 			unset( $args['related_terms'] );
 
 			$defaults = km_rpbt_get_default_args();
-			$this->debug[ 'function args'] = array_intersect_key ( $args , $defaults );
+			$this->debug[ 'function args'] = $args;
 			$this->debug[ 'related posts query'] = $query;
 
 			add_filter( 'related_posts_by_taxonomy', array( $this, 'posts_found' ) );
 
 			return $pieces;
 		}
+
 
 
 		/**
@@ -215,6 +247,7 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 		 * @return array Array with with post objects.
 		 */
 		function posts_found( $results ) {
+
 			if ( !empty( $results ) ) {
 				if ( isset( $results[0]->ID ) ) {
 					$this->debug[ 'related post ids found'] = wp_list_pluck( $results, 'ID' );
@@ -256,9 +289,9 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 		function wp_footer() {
 
 			$order = array(
-				'type', 'current post id', 'taxonomies used for related query',
-				'terms found for current post', 'terms used for related query',
-				'related post ids found',
+				'type', 'cached', 'current post id', 'taxonomies used for related query', 'cached taxonomies',
+				'terms found for current post', 'terms used for related query', 'cached terms',
+				'related post ids found', 'cached post ids',
 				'widget args', 'shortcode args', 'function args',
 				'related posts query',
 				'requested template', 'widget'
@@ -299,6 +332,19 @@ if ( !class_exists( 'Related_Posts_By_Taxonomy_Debug' ) ) {
 
 					// reorder debug array;
 					$debug_arr = array_merge( $_order, $debug_arr );
+
+					if ( isset( $debug_arr[ 'cached' ] ) && ( $debug_arr[ 'cached' ] === 'TRUE' ) ) {
+						unset( $debug_arr['related post ids found'] );
+						unset( $debug_arr['terms used for related query'] );
+						unset( $debug_arr['terms found for current post'] );
+						unset( $debug_arr['taxonomies used for related query'] );
+						unset( $debug_arr['related posts query'] );
+					} else {
+						unset( $debug_arr['cached post ids'] );
+						unset( $debug_arr['cached terms'] );
+						unset( $debug_arr['cached taxonomies'] );
+						unset( $debug_arr['related posts cache query'] );
+					}
 
 					foreach ( $debug_arr as $key => $value ) {
 
