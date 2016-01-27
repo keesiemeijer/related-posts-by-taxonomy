f9506de67d76e644c1db3e22012cfa0a1349ba24
keesiemeijer <keesie.meijer@gmail.com>
2016-01-27 13:27:21 +0100

    Add test for km_rpbt_sanitize_args()

--- tests/test-functions.php
+++ tests/test-functions.php
@@ -26,6 +26,7 @@ class KM_RPBT_Functions_Tests extends WP_UnitTestCase {
 		$this->tax_2_terms = $posts['tax2_terms'];
 	}
 
+
 	/**
 	 * Test if km_rpbt_plugin() returns an object.
 	 *
@@ -56,6 +57,54 @@ class KM_RPBT_Functions_Tests extends WP_UnitTestCase {
 
 
 	/**
+	 * Test sanitizing arguments.
+	 */
+	function test_km_rpbt_sanitize_args() {
+		$expected = array(
+			'post_types' => array( 'post' ), 'posts_per_page' => 0, 'order' => '',
+			'fields' => '', 'limit_posts' => -1, 'limit_year' => 0,
+			'limit_month' => 0, 'orderby' => '',
+			'exclude_terms' => array( 1, 2, 3 ), 'include_terms' => array(),  'exclude_posts' => array(),
+			'post_thumbnail' => false, 'related' => false,
+		);
+
+		$args = array(
+			'post_types' => false, 'posts_per_page' => false, 'order' => false,
+			'fields' => false, 'limit_posts' => -1, 'limit_year' => 'false',
+			'limit_month' => false, 'orderby' => false,
+			'exclude_terms' => array( 1, 2, 'string', false, 3, 2 ), 'include_terms' => false, 'exclude_posts' => false,
+			'post_thumbnail' => 'false', 'related' => 'lalala',
+		);
+
+		$this->assertEquals( $expected, km_rpbt_sanitize_args( $args ) );
+	}
+
+
+	/**
+	 * Test sanitizing arguments for cache.
+	 */
+	function test_km_rpbt_sanitize_args_context() {
+		$expected = array(
+			'post_types' => 'post', 'posts_per_page' => 0, 'order' => '',
+			'fields' => '', 'limit_posts' => -1, 'limit_year' => 0,
+			'limit_month' => 0, 'orderby' => '',
+			'exclude_terms' => '1,2,3', 'include_terms' => '',  'exclude_posts' => '',
+			'post_thumbnail' => false, 'related' => false,
+		);
+
+		$args = array(
+			'post_types' => false, 'posts_per_page' => false, 'order' => false,
+			'fields' => false, 'limit_posts' => -1, 'limit_year' => 'false',
+			'limit_month' => false, 'orderby' => false,
+			'exclude_terms' => array( 1, 2, 'string', false, 3, 2 ), 'include_terms' => false, 'exclude_posts' => false,
+			'post_thumbnail' => 'false', 'related' => 'lalala',
+		);
+
+		$this->assertEquals( $expected, km_rpbt_sanitize_args( $args, 'cache' ) );
+	}
+
+
+	/**
 	 * Test validating post types.
 	 */
 	function test_km_rpbt_validate_post_types() {
@@ -63,8 +112,6 @@ class KM_RPBT_Functions_Tests extends WP_UnitTestCase {
 
 		$this->assertEquals( array( 'post' ), km_rpbt_validate_post_types( $post_types ) );
 	}
-
-
 
 
 	/**
