526a202d4e9e73111ad08d0cf51b919127a66070
keesiemeijer <keesie.meijer@gmail.com>
2017-03-26 14:36:11 +0200

    Use class alias for PHPUnit version 6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,10 @@ $_tests_dir = '/tmp/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
 
+if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
+	class_alias( 'PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase' ); 
+}
+
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../related-posts-by-taxonomy.php';
 }
