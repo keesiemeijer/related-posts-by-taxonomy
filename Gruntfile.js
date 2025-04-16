6da750878c51b3a3439b56b9aa285003ffa3cd88
keesiemeijer <keesie.meijer@gmail.com>
2025-04-16 11:13:19 +0200

    Rename master to main and update latest WP version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,169 +1,163 @@
-module.exports = function( grunt ) {
+module.exports = function (grunt) {
+    require("load-grunt-tasks")(grunt);
 
-	require( 'load-grunt-tasks' )( grunt );
+    ("use strict");
 
-	'use strict';
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
 
-	// Project configuration
-	grunt.initConfig( {
+        githash: {
+            main: {
+                options: {},
+            },
+        },
 
-		pkg: grunt.file.readJSON( 'package.json' ),
+        addtextdomain: {
+            options: {
+                textdomain: "related-posts-by-taxonomy",
+            },
+            target: {
+                files: {
+                    src: ["*.php", "**/*.php", "!node_modules/**", "!bin/**", "!vendor/**"],
+                },
+            },
+        },
 
-		githash: {
-			main: {
-				options: {},
-			}
-		},
+        makepot: {
+            target: {
+                options: {
+                    domainPath: "/lang",
+                    mainFile: "related-posts-by-taxonomy.php",
+                    potFilename: "related-posts-by-taxonomy.pot",
+                    potHeaders: {
+                        poedit: true,
+                        "x-poedit-keywordslist": true,
+                    },
+                    type: "wp-plugin",
+                    updateTimestamp: true,
+                },
+            },
+        },
 
-		addtextdomain: {
-			options: {
-				textdomain: 'related-posts-by-taxonomy',
-			},
-			target: {
-				files: {
-					src: [ '*.php', '**/*.php', '!node_modules/**', '!bin/**', '!vendor/**' ]
-				}
-			}
-		},
+        uglify: {
+            options: {
+                banner:
+                    "/*\n" + " * " + "<%= pkg.name %>\n" + " * " + "v<%= pkg.version %>\n" + " * " + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + " **/\n",
+            },
 
-		makepot: {
-			target: {
-				options: {
-					domainPath: '/lang',
-					mainFile: 'related-posts-by-taxonomy.php',
-					potFilename: 'related-posts-by-taxonomy.pot',
-					potHeaders: {
-						poedit: true,
-						'x-poedit-keywordslist': true
-					},
-					type: 'wp-plugin',
-					updateTimestamp: true
-				}
-			}
-		},
+            target: {
+                files: {
+                    "includes/assets/js/lazy-loading.min.js": ["includes/assets/js/lazy-loading.js"],
+                },
+            },
+        },
 
-		uglify: {
-			options: {
-				banner: '/*\n' +
-					' * ' + '<%= pkg.name %>\n' +
-					' * ' + 'v<%= pkg.version %>\n' +
-					' * ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-					' **/\n'
-			},
+        // Clean up build directory
+        clean: {
+            main: ["build/<%= pkg.name %>"],
+            release: [
+                "**",
+                ".travis.yml",
+                ".gitignore",
+                ".git/**",
+                "!lang/**",
+                "!templates/**",
+                "!includes/**",
+                "!related-posts-by-taxonomy.php",
+                "!readme.txt",
+            ],
+        },
 
-			target: {
-				files: {
-					'includes/assets/js/lazy-loading.min.js': [ 'includes/assets/js/lazy-loading.js' ]
-				}
-			}
-		},
+        // Copy the theme into the build directory
+        copy: {
+            main: {
+                src: [
+                    "**",
+                    "!node_modules/**",
+                    "!vendor/**",
+                    "!bin/**",
+                    "!tests/**",
+                    "!build/**",
+                    "!editor-block/**",
+                    "!webpack.config.js",
+                    "!Gruntfile.js",
+                    "!package.json",
+                    "!package-lock.json",
+                    "!composer.json",
+                    "!composer.lock",
+                    "!phpunit.xml",
+                    "!README.md",
+                    "!readme.md",
+                    "!travis.yml",
+                    "!.git/**",
+                    "!.github/**",
+                    "!.gitignore",
+                    "!.gitmodules",
+                    "!.gitattributes",
+                    "!.editorconfig",
+                ],
+                dest: "build/<%= pkg.name %>/",
+            },
+        },
 
-		// Clean up build directory
-		clean: {
-			main: [ 'build/<%= pkg.name %>' ],
-			release: [
-				'**',
-				'.travis.yml',
-				'.gitignore',
-				'.git/**',
-				'!lang/**',
-				'!templates/**',
-				'!includes/**',
-				'!related-posts-by-taxonomy.php',
-				'!readme.txt'
-			]
-		},
+        version: {
+            readmetxt: {
+                options: {
+                    prefix: "Stable tag: *",
+                },
+                src: ["readme.txt"],
+            },
+            tested_up_to: {
+                options: {
+                    pkg: {
+                        version: "<%= pkg.tested_up_to %>",
+                    },
+                    prefix: "Tested up to: *",
+                },
+                src: ["readme.txt", "readme.md"],
+            },
+            requires_at_least: {
+                options: {
+                    pkg: {
+                        version: "<%= pkg.requires_at_least %>",
+                    },
+                    prefix: "Requires at least: *",
+                },
+                src: ["readme.txt", "readme.md"],
+            },
+            plugin: {
+                options: {
+                    prefix: "Version: *",
+                },
+                src: ["readme.md", "related-posts-by-taxonomy.php"],
+            },
+        },
 
-		// Copy the theme into the build directory
-		copy: {
-			main: {
-				src: [
-					'**',
-					'!node_modules/**',
-					'!vendor/**',
-					'!bin/**',
-					'!tests/**',
-					'!build/**',
-					'!editor-block/**',
-					'!webpack.config.js',
-					'!Gruntfile.js',
-					'!package.json',
-					'!package-lock.json',
-					'!composer.json',
-					'!composer.lock',
-					'!phpunit.xml',
-					'!README.md',
-					'!readme.md',
-					'!travis.yml',
-					'!.git/**',
-					'!.github/**',
-					'!.gitignore',
-					'!.gitmodules',
-					'!.gitattributes',
-					'!.editorconfig',
-				],
-				dest: 'build/<%= pkg.name %>/'
-			}
-		},
+        replace: {
+            replace_branch: {
+                src: ["readme.md"],
+                overwrite: true, // overwrite matched source files
+                replacements: [
+                    {
+                        from: /related-posts-by-taxonomy.svg\?branch=(main|develop)/g,
+                        to: "related-posts-by-taxonomy.svg?branch=<%= githash.main.branch %>",
+                    },
+                ],
+            },
+        },
+    });
 
-		version: {
-			readmetxt: {
-				options: {
-					prefix: 'Stable tag: *'
-				},
-				src: [ 'readme.txt' ]
-			},
-			tested_up_to: {
-				options: {
-					pkg: {
-						"version": "<%= pkg.tested_up_to %>"
-					},
-					prefix: 'Tested up to: *'
-				},
-				src: [ 'readme.txt', 'readme.md' ]
-			},
-			requires_at_least: {
-				options: {
-					pkg: {
-						"version": "<%= pkg.requires_at_least %>"
-					},
-					prefix: 'Requires at least: *'
-				},
-				src: [ 'readme.txt', 'readme.md' ]
-			},
-			plugin: {
-				options: {
-					prefix: 'Version: *'
-				},
-				src: [ 'readme.md', 'related-posts-by-taxonomy.php' ]
-			},
-		},
+    grunt.registerTask("i18n", ["addtextdomain", "makepot"]);
 
-		replace: {
-			replace_branch: {
-				src: [ 'readme.md' ],
-				overwrite: true, // overwrite matched source files
-				replacements: [ {
-					from: /related-posts-by-taxonomy.svg\?branch=(master|develop)/g,
-					to: "related-posts-by-taxonomy.svg?branch=<%= githash.main.branch %>"
-				} ]
-			}
-		}
+    grunt.registerTask("travis", ["githash", "replace:replace_branch"]);
 
-	} );
+    // Creates build
+    grunt.registerTask("build", ["clean:main", "uglify", "version", "makepot", "travis", "copy:main"]);
 
+    // Removes ALL development files in the root directory
+    // !!! be careful with this
+    grunt.registerTask("release", ["version", "clean:release"]);
 
-	grunt.registerTask( 'i18n', [ 'addtextdomain', 'makepot' ] );
-
-	grunt.registerTask( 'travis', [ 'githash', 'replace:replace_branch' ] );
-
-	// Creates build
-	grunt.registerTask( 'build', [ 'clean:main', 'uglify', 'version', 'makepot', 'travis', 'copy:main' ] );
-
-	// Removes ALL development files in the root directory
-	// !!! be careful with this
-	grunt.registerTask( 'release', [ 'version', 'clean:release', ] );
-
-	grunt.util.linefeed = '\n';
-
+    grunt.util.linefeed = "\n";
 };
